d5e5d07b392b0c6e737fc5db3aa7ffb8b8fb8a3d
David Trichet <dtrichet@yahoo.com>
2015-08-27 17:51:38 +0200

    Update_20150827_01
    Update_20150827_01

--- lance.js
+++ lance.js
@@ -1,8 +1,10 @@
-// GESTION DES TEMPLATES ET DES VUES
-// ----------------------------------------------
+// ----------------------------------------------------------------------------
+// // GESTION DES TEMPLATES ET DES VUES
+// ----------------------------------------------------------------------------
 var express = require('express');
 var app = express();
 var bodyParser  = require('body-parser');
+var mongoose = require('mongoose');
 var count = 0;
 
 app.use(express.static(__dirname + '/public'));
@@ -10,16 +12,40 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
-
-// set the view engine to jade
 app.set('view engine', 'jade');
+// ----------------------------------------------------------------------------
+// Accès aux données
+// ----------------------------------------------------------------------------
+// mongoose.connect('mongodb://frankenstein:zombi>@ds055762.mongolab.com:55762/monsters');
+mongoose.connect('mongodb://localhost/monsters');
+var db = mongoose.connection;
+db.on('error', function(error) {
+    console.log('WARNING ***** DB connection error *****');
+    console.log('WARNING ***** ' + error);
+    });
+db.on('open', function() {
+    console.log('INFOS ***** DB connection OK *****');
+    });
 
+var monsterSchema = mongoose.Schema({
+    name: String,
+    desc: String,
+    level: Number
+});
+var Monster = mongoose.model('Monster', monsterSchema);
+
+/*
 var monsters = [
 	{name: 'Riri', level:3, desc: 'le petit cochon qui rit'},
 	{name: 'Fifi', level:2, desc: 'le vampire qui rend pire'},
 	{name: 'Loulou', level:6, desc: 'le loup garou qui fait des trous'}
 ];
+*/
 
+// ----------------------------------------------------------------------------
+// Gestion des URL
+// ----------------------------------------------------------------------------
+// 
 // Gestion URL de base du site
 app.get('/home', function (req, res) {
 	res.render('home.jade');
@@ -27,7 +53,13 @@ app.get('/home', function (req, res) {
 
 // Gestion URL de base du site
 app.get('/monsters', function (req, res) {
-	res.render('monsters_list.jade', {monsters: monsters});
+    Monster.find(function(err, monsters) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('monsters_list.jade', {monsters: monsters});
+        }
+    });
 });
 
 // Gestion URL nouveau monstre
@@ -37,22 +69,33 @@ app.get('/new_monster', function (req, res) {
 
 // Gestion des monstres URL /monsters/id
 app.get('/monster/:id', function (req, res) {
-	var monsterId = monsters[req.params.id - 1];
-	count += 1;	
-	res.render('monster.jade', {monster: monsterId});
+    Monster.findById(req.params.id, function(err, monster) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('monster.jade', {monster: monster});
+        }
+    });
 });
 
 // Creation des monstres Methode POST dans le formulaire req.body
 app.post('/create_monster', function (req, res){
-	monsters.push({
-		name: req.body.name,
-		desc: req.body.desc,
-		level: parseInt(req.body.level, 10)
-	});
-	res.send("OK, monstre validé");
+    var monster = new Monster({
+	name: req.body.name,
+	desc: req.body.desc,
+	level: parseInt(req.body.level, 10)
+    });
+    monster.save(function (err, monster) {
+        if (err) {
+            console.log("Can't save Monster " + err);
+        } else {
+            res.render('monster_created.jade');
+        }
+    });
 });
-
-// ----------------------------------------
+// ----------------------------------------------------------------------------
+// SERVEUR WEB
+// ----------------------------------------------------------------------------
 var server = app.listen(8888, function () {
 	var host = server.address().address;
 	var port = server.address().port;
